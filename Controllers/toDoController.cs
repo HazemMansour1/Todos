6f25238b39b4dcdfe8a5679c123f1cc1324299f3
HazemMansour <mansahmed9@gmail.com>
2024-07-28 19:31:20 +0300

    Add project files.

---
+++ Controllers/toDoController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class toDoController : ControllerBase
+    {
+        private static List<toDo> toDos = new List<toDo>();
+        private static int num = 1;
+        [HttpGet("getAllTasks")]
+        public IActionResult getAllTask()
+        {
+            return Ok(toDos);
+        }
+        [HttpGet("getTaskByNum")]
+
+        public IActionResult getTaskByNum(int num)
+        {
+            var tsk = toDos.SingleOrDefault(t => t.taskNum == num);
+            if (tsk == null)
+            {
+                return NotFound();
+            }
+            return Ok(tsk);
+        }
+
+        [HttpPost("addTask")]
+        public IActionResult addTask(toDo tsk)
+        {
+            tsk.taskNum = num++;
+            toDos.Add(tsk);
+            return CreatedAtAction(nameof(getTaskByNum),new {num = tsk.taskNum},tsk);
+        }
+        [HttpDelete("deleteTask")]
+
+        public IActionResult deleteTask(int n)
+        {
+            var tsk = toDos.SingleOrDefault(t => t.taskNum == n);
+            if (tsk == null)
+            {
+                return NotFound();
+            }
+            toDos.Remove(tsk);
+            return NoContent();
+        }
+        [HttpPut("updateTask")]
+        public IActionResult UpdateEmployee(toDo updatedTask)
+        {
+            var tsk = toDos.SingleOrDefault(t => t.taskNum == updatedTask.taskNum);
+            if (tsk == null)
+            {
+                return NotFound();
+            }
+
+            tsk.taskName = updatedTask.taskName;
+            tsk.taskDue = updatedTask.taskDue;
+            tsk.done = updatedTask.done;
+            return Ok(tsk);
+
+        }
+    }
+}
---
+++ toDo.cs
@@ -0,0 +1,14 @@
+﻿namespace Todos
+{
+    public class toDo
+    {
+        public int taskNum { get; set; }
+
+        public string taskName { get; set; }
+
+        public string taskDue { get; set; }
+
+        public Boolean done { get; set; }
+
+    }
+}
